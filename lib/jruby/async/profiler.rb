871cae4c1b81efa64838d15835283d66313c8741
Charles Oliver Nutter <headius@headius.com>
2019-11-04 10:51:04 -0600

    Initial commit.

---
+++ lib/jruby/async/profiler.rb
@@ -0,0 +1,10 @@
+require "jruby/async/profiler/version"
+
+module Jruby
+  module Async
+    module Profiler
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jruby/async/profiler/version.rb
@@ -0,0 +1,7 @@
+module Jruby
+  module Async
+    module Profiler
+      VERSION = "0.1.0"
+    end
+  end
+end
